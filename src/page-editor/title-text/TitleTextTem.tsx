570793e56da7875f3dbbf43e6eb3cd2e81b2b40d
冯杰 <986852048@qq.com>
2024-04-30 16:29:16 +0800

    feat: demo commit

---
+++ src/page-editor/title-text/TitleTextTem.tsx
@@ -0,0 +1,5 @@
+const Index = () => {
+  return <div>标题文本 渲染组件</div>
+}
+
+export default Index
---
+++ src/page-editor/title-text/index.tsx
@@ -0,0 +1,15 @@
+// const TemplateEngine = () => {
+//   return (
+//     <>
+//       <div>模板引擎</div>
+//     </>
+//   )
+// }
+
+export default {
+  name: '标题文本',
+  icon: 'icon-titletext',
+  groupType: 'titletext',
+  temModule: 'TitleTextTem',
+  setModule: 'TitleTextSet',
+}

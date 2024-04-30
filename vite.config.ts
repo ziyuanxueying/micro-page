65409d0cb31dff30f1df1ef0f4b5c9d1d45ad333
lixiaolong <im.xlsama@gmail.com>
2024-04-30 18:51:13 +0800

    chore: update

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import UnoCSS from 'unocss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    open: true,
+  },
   plugins: [
     AutoImport({
       include: [/\.[tj]sx?$/],

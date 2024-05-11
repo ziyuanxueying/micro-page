c03a7be803ffa600a1fae8cad5aa36948d14e259
冯杰 <986852048@qq.com>
2024-05-11 17:28:54 +0800

    feat: test

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import react from '@vitejs/plugin-react-swc'
 import AutoImport from 'unplugin-auto-import/vite'
 import { defineConfig } from 'vite'
 import { resolve } from 'path'
+import dts from 'vite-plugin-dts'
 // https://vitejs.dev/config/
 export default ({ mode }: { mode: string }) => {
   return defineConfig({
@@ -10,6 +11,7 @@ export default ({ mode }: { mode: string }) => {
       open: false, // 设置服务启动时是否自动打开浏览器
     },
     plugins: [
+      dts(),
       AutoImport({
         include: [/\.[tj]sx?$/],
         imports: [
@@ -30,24 +32,25 @@ export default ({ mode }: { mode: string }) => {
       lib:
         mode === 'lib'
           ? {
-              entry: resolve(__dirname, 'src/index.tsx'),
+              entry: resolve(__dirname, 'src/index.ts'),
               name: '@wd/micro-page',
               fileName: 'index',
               // formats: ['cjs'],
             }
           : undefined,
       rollupOptions: {
-        // input: {
-        //   main: resolve(__dirname, 'src/main.tsx'),
-        // },
-        // external: ['vue'],
+        // input: 'src/index.ts',
         // output: {
-        //   assetFileNames: 'my-library.css',
-        //   exports: 'named',
-        //   globals: {
-        //     vue: 'Vue',
-        //   },
+        //   format: 'es',
+        //   dir: 'dist',
         // },
+        external: ['react', 'react-dom'],
+        output: {
+          globals: {
+            react: 'React',
+            'react-dom': 'ReactDOM',
+          },
+        },
       },
     },
   })

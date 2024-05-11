45b51986032b4d5d99379110381cb19e1e3e0828
冯杰 <986852048@qq.com>
2024-05-11 18:09:15 +0800

    feat: test

--- vite.config.ts
+++ vite.config.ts
@@ -29,7 +29,7 @@ export default ({ mode }: { mode: string }) => {
     ],
     build: {
       outDir: mode === 'lib' ? 'dist/lib' : 'dist/app',
-      target: 'node',
+      target: 'node18.18.0',
       lib:
         mode === 'lib'
           ? {

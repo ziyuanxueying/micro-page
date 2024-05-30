815d00336dd17c5ed37e3e7a8da9318a2fe9c6db
lixiaolong <im.xlsama@gmail.com>
2024-05-30 16:41:19 +0800

    feat: update float btn

--- src/page-editor/metas/float-btn/FloatBtnSet.tsx
+++ src/page-editor/metas/float-btn/FloatBtnSet.tsx
@@ -1,38 +1,118 @@
-import { Form, Input } from 'antd'
+import useStore from '@/store'
+import { Divider, Form, Input, Radio, Typography, Switch, InputNumber } from 'antd'
+import { WdUploadPicture } from '@wd/component-ui'
+import { toComponentUrl } from '@/utils'
 
-const { TextArea } = Input
+const { Title } = Typography
 
 const FloatBtnSet = () => {
+  const { components, selectedComponentId, updateComponentData } = useStore()
+  const selectedComponent = components.find(c => c.id === selectedComponentId)
+
+  const [form] = Form.useForm()
+
+  const { buttonImgUrl, modalImgUrl } = selectedComponent?.data || {}
+
+  const buttonImgList = buttonImgUrl ? [{ url: buttonImgUrl }] : []
+  const modalImgList = modalImgUrl ? [{ url: modalImgUrl }] : []
+
+  const onClickTypeChange = () => {
+    const { clickType, ...values } = form.getFieldsValue()
+
+    const next: any = { clickType, ...values }
+
+    if (clickType === 'modal') {
+      next.link = ''
+    } else {
+      next.preview = false
+      next.modalImgUrl = ''
+    }
+
+    form.setFieldsValue(next)
+    updateComponentData(selectedComponentId, {
+      ...next,
+      buttonImgUrl: toComponentUrl(next.buttonImgUrl),
+      modalImgUrl: toComponentUrl(next.modalImgUrl),
+    })
+  }
+
   return (
-    <Form labelCol={{ span: 5 }}>
-      <Form.Item label="标题内容">
-        <Input required />
-      </Form.Item>
-      <Form.Item label="描述内容">
-        <TextArea />
-      </Form.Item>
-      <Form.Item label="倒角">
-        <Input />
-      </Form.Item>
-      <Form.Item label="显示位置">
-        <Input />
-      </Form.Item>
-      <Form.Item label="标题粗细">
-        <Input />
-      </Form.Item>
-      <Form.Item label="描述粗细">
-        <Input />
-      </Form.Item>
-      <Form.Item label="标题颜色">
-        <Input />
-      </Form.Item>
-      <Form.Item label="描述颜色">
-        <Input />
-      </Form.Item>
-      <Form.Item label="背景颜色">
-        <Input />
-      </Form.Item>
-    </Form>
+    <>
+      <Title level={5} style={{ fontWeight: 500, marginBottom: 0 }} css={css({ textIndent: 10 })}>
+        浮标
+      </Title>
+      <Divider css={css({ margin: '16px 0' })} />
+      <Form
+        form={form}
+        labelCol={{ span: 5 }}
+        initialValues={selectedComponent?.data}
+        onValuesChange={(_, allValues) => {
+          updateComponentData(selectedComponentId, {
+            ...allValues,
+            buttonImgUrl: toComponentUrl(allValues.buttonImgUrl),
+            modalImgUrl: toComponentUrl(allValues.modalImgUrl),
+          })
+        }}
+      >
+        <Form.Item label="按钮图片" name="buttonImgUrl" required>
+          <WdUploadPicture
+            url="/cos-api/xapi-pc-web/file/tmpSecret"
+            cosType="QD"
+            fileList={buttonImgList}
+            path="wxxcx/img"
+            multiple={false}
+            maxCount={1}
+            theme="drag"
+            defaultTip="更换图片"
+            width={100}
+            height={100}
+          />
+        </Form.Item>
+        <Form.Item label="按钮位置" name="top">
+          <InputNumber />
+        </Form.Item>
+        <Form.Item label="点击效果" name="clickType" required>
+          <Radio.Group onChange={onClickTypeChange}>
+            <Radio value="modal">弹窗</Radio>
+            <Radio value="link">页面跳转</Radio>
+          </Radio.Group>
+        </Form.Item>
+        <Form.Item noStyle shouldUpdate={(prev, cur) => prev.clickType !== cur.clickType}>
+          {({ getFieldValue }) => {
+            const clickType = getFieldValue('clickType')
+            if (clickType === 'modal') {
+              return (
+                <>
+                  <Form.Item label="弹窗图片" name="modalImgUrl">
+                    <WdUploadPicture
+                      url="/cos-api/xapi-pc-web/file/tmpSecret"
+                      cosType="QD"
+                      fileList={modalImgList}
+                      path="wxxcx/img"
+                      multiple={false}
+                      maxCount={1}
+                      theme="drag"
+                      defaultTip="更换图片"
+                      width={100}
+                      height={100}
+                    />
+                  </Form.Item>
+                  <Form.Item label="弹窗预览" name="preview">
+                    <Switch />
+                  </Form.Item>
+                </>
+              )
+            }
+
+            return (
+              <Form.Item label="跳转链接" name="link">
+                <Input />
+              </Form.Item>
+            )
+          }}
+        </Form.Item>
+      </Form>
+    </>
   )
 }
 
--- src/page-editor/metas/float-btn/index.ts
+++ src/page-editor/metas/float-btn/index.ts
@@ -5,8 +5,15 @@ export default {
   group: '基础组件',
   icon: 'icon-float-btn',
   metaType: 'bas-floatBtn',
-  temModule: 'floatBtnTem',
-  setModule: 'floatBtnSet',
+  temModule: 'FloatBtnTem',
+  setModule: 'FloatBtnSet',
   order: 8,
-  data: {},
+  data: {
+    buttonImgUrl: '',
+    top: 345,
+    clickType: 'modal',
+    modalImgUrl: '',
+    link: '',
+    preview: false,
+  },
 } as Component

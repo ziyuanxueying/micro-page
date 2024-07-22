c556e5a00cdb3c33527afbd6e8b67fd08a173ec0
sl-w40 <wangsl_@outlook.com>
2024-07-22 20:23:28 +0800

    feat: 预览

--- src/page-editor/index.tsx
+++ src/page-editor/index.tsx
@@ -8,6 +8,10 @@ import { Button, Space } from 'antd'
 // import { updateJson, findByIdForB, getCoupons } from '@/api'
 import { findByIdForB, updateJson } from '@/api'
 import useStore, { ActionEnums, Component, pageType } from '@/store'
+import { WdModal, WdPlazaSelect } from '@wd/component-ui'
+import QRCode from '../utils/qrcode.js'
+import './index.less'
+
 type dataType = {
   components: Component[]
   pageConfig: pageType
@@ -16,6 +20,7 @@ type dataType = {
 //CP0811283496616108032,微页面自测
 //CP0811527827121074176,全量自测
 const TemplateEngine = () => {
+  const [preview, setPreview] = React.useState<any>({})
   const { components, pageConfig, action, updateComponents, updatePageConfig } = useStore()
   const handleSave = async () => {
     // const data = await createJson({
@@ -40,6 +45,37 @@ const TemplateEngine = () => {
       updatePageConfig(data.content.pageConfig)
     }, 1000)
   }
+
+  const showPreviewCode = useCallback((item: any) => {
+    // https://api.wandacm.com.cn/qre  体验版
+    // https://api.wandacm.com.cn/qr 正式版
+    setPreview({
+      show: true,
+      id: item.id,
+      onCancel: () => setPreview({}),
+    })
+  }, [])
+
+  const changePlaza = useCallback(
+    (value: any) => {
+      if (value.length <= 3) return
+      const svgQRCode = QRCode({
+        msg: `https://api.wandacm.com.cn/qre?key=MicroPageIndex&plazaId=${value[3]}&templateId=${preview.id}`,
+        dim: 762,
+        pad: 7,
+        mtx: 7,
+        ecl: 'H',
+        pal: ['#000000', '#ffffff'],
+      })
+      setPreview({
+        qrCode: svgQRCode.outerHTML,
+        show: true,
+        onCancel: () => setPreview({}),
+      })
+    },
+    [preview],
+  )
+
   useEffect(() => {
     findById()
   }, [])
@@ -65,12 +101,51 @@ const TemplateEngine = () => {
         </main>
       </DndProvider>
       <Space css={css([flexrc, { padding: '10px' }])}>
+        <Button type="primary" onClick={() => showPreviewCode({ id: 1 })}>
+          预览
+        </Button>
         <Button type="primary" onClick={handleSave}>
           保存
         </Button>
         <Button onClick={findById}>查询</Button>
         <Button>取消</Button>
       </Space>
+
+      <WdModal
+        className="preview-modal"
+        modalProps={{
+          size: 'small',
+          title: '预览',
+          footer: false,
+          onCancel: preview.onCancel,
+        }}
+        open={preview.show}
+      >
+        <Space direction="vertical">
+          <Space>
+            <div>选择广场：</div>
+            <WdPlazaSelect onChange={changePlaza} />
+          </Space>
+          {preview.qrCode && (
+            <>
+              <div>
+                <p style={{ fontSize: 18, margin: 0, marginTop: 25 }}>使用微信扫一扫预览页面</p>
+                <div
+                  dangerouslySetInnerHTML={{ __html: preview.qrCode }}
+                  className="qr-code-box"
+                ></div>
+              </div>
+              <div className="preview-tips">小程序预览</div>
+              <b style={{ marginTop: 20 }}>温馨提示</b>
+              <div>
+                <span>此二维码仅做页面预览使用</span>
+                <div></div>
+                <span>预览页面默认展示当前广场数据</span>
+              </div>
+            </>
+          )}
+        </Space>
+      </WdModal>
     </div>
   )
 }
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,14 @@
 /// <reference types="vite/client" />
+
+interface QRCodeOptions {
+  msg: string
+  dim?: number
+  pad?: number
+  mtx?: number
+  ecl?: string
+  ecb?: number
+  pal?: Array<string>
+  vrb?: number
+}
+
+declare function QRCode(options: QRCodeOptions): HTMLElement
